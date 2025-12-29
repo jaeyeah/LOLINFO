a2a517506264c909bd66eebef58f5f3e829a7719
jaeyeah <limjh819@naver.com>
2025-12-29 17:43:55 +0900

    프로젝트 생성 및 git 연동

---
+++ lolinfo/src/main/java/com/lol/lolinfo/LolinfoApplication.java
@@ -0,0 +1,13 @@
+package com.lol.lolinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolinfoApplication.class, args);
+	}
+
+}
---
+++ lolinfo/src/test/java/com/lol/lolinfo/LolinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lol.lolinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
