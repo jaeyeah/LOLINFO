50b70ae53209d528ea1cc17646fc5c870f8a7279
jaeyeah <limjh819@naver.com>
2025-12-30 20:56:07 +0900

    cors 오류 해결중

--- lolinfo/src/main/java/com/lol/lolinfo/LolinfoApplication.java
+++ lolinfo/src/main/java/com/lol/lolinfo/LolinfoApplication.java
@@ -2,12 +2,12 @@ package com.lol.lolinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class LolinfoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LolinfoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LolinfoApplication.class, args);
+    }
 }
