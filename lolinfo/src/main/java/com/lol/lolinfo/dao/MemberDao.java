9f83dc1a2b0e590651a0df62d892ac966a47089a
jaeyeah <limjh819@naver.com>
2026-01-13 16:34:24 +0900

    아이디/닉네임 중복검사

--- lolinfo/src/main/java/com/lol/lolinfo/dao/MemberDao.java
+++ lolinfo/src/main/java/com/lol/lolinfo/dao/MemberDao.java
@@ -1,5 +1,7 @@
 package com.lol.lolinfo.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,7 +14,30 @@ public class MemberDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	/// 등록(회원가입)
 	public void insert(MemberDto memberDto) {
+		// 비밀번호 암호화
+		// 등록
 		sqlSession.insert("member.insert", memberDto);
 	}
+	
+	///조회
+	// 기본조회(목록)
+	public List<MemberDto> selectList(){
+		return sqlSession.selectOne("member.selectList");
+	}
+	// 상세조회 (+아이디 중복 검사)
+	public MemberDto selectOne(String memberId) {
+		return sqlSession.selectOne("member.detail", memberId);
+	}
+	
+	// 닉네임 중복 검사용 조회
+	public MemberDto selectOneByMemberNickname(String memberNickname) {
+		return sqlSession.selectOne("member.detailByNickname", memberNickname);
+	}
+	
+	
+	
+	
+	
 }
--- lolinfo/src/main/java/com/lol/lolinfo/restcontroller/MemberRestController.java
+++ lolinfo/src/main/java/com/lol/lolinfo/restcontroller/MemberRestController.java
@@ -1,7 +1,11 @@
 package com.lol.lolinfo.restcontroller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +22,27 @@ public class MemberRestController {
 	@Autowired
 	private MemberDao memberDao;
 
+	//회원가입
 	@PostMapping("/")
 	public void insert(@RequestBody MemberDto memberDto) {
 		memberDao.insert(memberDto);
 	}
+	// 회원가입 - 아이디 중복검사
+	@GetMapping("/memberId/{memberId}")
+	public boolean selectOne(@PathVariable String memberId) {
+		MemberDto memberDto = memberDao.selectOne(memberId);
+		return memberDto == null;
+	}
+	// 회원가입 - 닉네임 중복검사
+	@GetMapping("/memberNickname/{memberNickname}")
+	public boolean checkMemberNickname(@PathVariable String memberNickname) {
+		MemberDto memberDto = memberDao.selectOneByMemberNickname(memberNickname);
+		return memberDto == null;
+	}
 	
+	//회원 조회
+	@GetMapping("/")
+	public List<MemberDto> selectList(){
+		return memberDao.selectList();
+	}
 }
