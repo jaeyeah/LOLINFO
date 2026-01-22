a4396908e0379ae1bef75916acb00aef2e7f2f55
jaeyeah <limjh819@naver.com>
2026-01-22 15:14:03 +0900

    팀 삭제 매핑 추가

--- lolinfo/src/main/java/com/lol/lolinfo/restcontroller/TeamRestController.java
+++ lolinfo/src/main/java/com/lol/lolinfo/restcontroller/TeamRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,5 +73,10 @@ public class TeamRestController {
 		System.out.println("팀 수정 실행");
 	}
 
-	
+	//삭제
+	@DeleteMapping("/")
+	public void delete(@PathVariable int teamId) {
+		teamDao.delete(teamId);
+		System.out.println("팀 수정 실행");
+	}
 }
