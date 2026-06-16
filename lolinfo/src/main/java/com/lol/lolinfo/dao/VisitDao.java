89faca3e91f65cf1860d60b98a6eb52e7a236be0
jaeyeah <limjh819@naver.com>
2026-06-16 19:38:06 +0900

    관리자 - 방문통계 목록 백엔드 구현

--- lolinfo/src/main/java/com/lol/lolinfo/dao/VisitDao.java
+++ lolinfo/src/main/java/com/lol/lolinfo/dao/VisitDao.java
@@ -1,10 +1,13 @@
 package com.lol.lolinfo.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.lol.lolinfo.dto.VisitDto;
+import com.lol.lolinfo.vo.VisitListVO;
 
 @Repository
 public class VisitDao {
@@ -19,6 +22,10 @@ public class VisitDao {
 	//수정 - 로그인시 변경
 	public boolean updateLogin(VisitDto visitDto) {
 		return sqlSession.update("visit.updateLogin",visitDto) > 0;	
+	}
+	//조회
+	public List<VisitListVO> selectList(){
+		return sqlSession.selectList("visit.selectList");
 	}
 
 
--- lolinfo/src/main/java/com/lol/lolinfo/restcontroller/VisitRestController.java
+++ lolinfo/src/main/java/com/lol/lolinfo/restcontroller/VisitRestController.java
@@ -1,14 +1,19 @@
 package com.lol.lolinfo.restcontroller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lol.lolinfo.dao.VisitDao;
 import com.lol.lolinfo.dto.VisitDto;
 import com.lol.lolinfo.service.VisitService;
+import com.lol.lolinfo.vo.VisitListVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,11 +25,23 @@ public class VisitRestController {
 
 	@Autowired
 	private VisitService visitService;
+	@Autowired
+	private VisitDao visitDao;
+	
 	
 	//등록
 	@PostMapping("/")
 	public void insert(@RequestBody VisitDto visitDto) {
 		visitService.insert(visitDto);
+	}
+	
+	//로그인시 변경
+	
+	
+	//조회
+	@GetMapping("/")
+	public List<VisitListVO> selectList(){
+		return visitDao.selectList();
 	}
 	
 }
