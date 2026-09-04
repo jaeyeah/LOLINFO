e24818932c466ec0dd86144b3adca5ccb219c74d
jaeyeah <limjh819@naver.com>
2026-09-04 09:50:06 +0900

    게시판 목록 VO 적용(닉네임 추가)

--- lolinfo/src/main/java/com/lol/lolinfo/dao/BoardDao.java
+++ lolinfo/src/main/java/com/lol/lolinfo/dao/BoardDao.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.lol.lolinfo.dto.BoardDto;
+import com.lol.lolinfo.vo.BoardListVO;
 
 @Repository
 public class BoardDao {
@@ -25,6 +26,10 @@ public class BoardDao {
 	public List<BoardDto> selectList(){
 		return sqlSession.selectList("board.selectList");
 	}
+	public List<BoardListVO> selectBoardList(){
+		return sqlSession.selectList("board.selectBoardList");
+	}
+	
 	// 상세
 	public BoardDto selectOne(int boardId) {
 		return sqlSession.selectOne("board.selectOne",boardId);
