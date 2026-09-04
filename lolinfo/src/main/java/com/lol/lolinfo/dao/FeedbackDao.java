3b9970dca7af5d63da7065e37893745b0596fa34
jaeyeah <limjh819@naver.com>
2026-09-04 14:56:35 +0900

    피드백 목록 구현

--- lolinfo/src/main/java/com/lol/lolinfo/dao/FeedbackDao.java
+++ lolinfo/src/main/java/com/lol/lolinfo/dao/FeedbackDao.java
@@ -1,10 +1,13 @@
 package com.lol.lolinfo.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.lol.lolinfo.dto.FeedbackDto;
+import com.lol.lolinfo.vo.PageVO;
 
 @Repository
 public class FeedbackDao {
@@ -12,8 +15,18 @@ public class FeedbackDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	
+	// 등록
 	public void insert(FeedbackDto feedbackDto) {
 		sqlSession.insert("feedback.insert",feedbackDto);
+	}
+	
+	// 목록조회
+	public List<FeedbackDto> selectList(PageVO pageVO){
+		return sqlSession.selectList("feedback.selectList",pageVO);
+	}
+	public int count() {
+		return sqlSession.selectOne("feedback.count");
 	}
 	
 	
