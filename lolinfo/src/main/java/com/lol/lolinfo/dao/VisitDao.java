96d70e6e3c272d67d18a56e67334d029f88689cb
jaeyeah <limjh819@naver.com>
2026-09-07 14:05:30 +0900

    메인페이지 방문 및 통계 백엔드 구현

--- lolinfo/src/main/java/com/lol/lolinfo/dao/VisitDao.java
+++ lolinfo/src/main/java/com/lol/lolinfo/dao/VisitDao.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.lol.lolinfo.dto.VisitDto;
+import com.lol.lolinfo.vo.HomeStatsVO;
 import com.lol.lolinfo.vo.VisitListVO;
 
 @Repository
@@ -32,7 +33,10 @@ public class VisitDao {
 		return sqlSession.selectList("visit.selectYear",year);
 	}
 
-
+	// 홈화면 조회용
+	public HomeStatsVO selectHomeStats() {
+	    return sqlSession.selectOne("visit.selectHomeStats");
+	}
 
 
 }
