7da5e929481a104f38b6267e2631122bbf90c14d
jaeyeah <limjh819@naver.com>
2026-09-09 10:35:06 +0900

    게시글 수정/삭제 기능 백엔드 구현

--- lolinfo/src/main/java/com/lol/lolinfo/restcontroller/BoardRestController.java
+++ lolinfo/src/main/java/com/lol/lolinfo/restcontroller/BoardRestController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import com.lol.lolinfo.dao.BoardDao;
 import com.lol.lolinfo.dto.BoardDto;
 import com.lol.lolinfo.service.BoardService;
 import com.lol.lolinfo.vo.BoardListVO;
+import com.lol.lolinfo.vo.ScrimUpdateVO;
 
 @CrossOrigin
 @RestController
@@ -43,4 +46,21 @@ public class BoardRestController {
 	public BoardDto list(@PathVariable int boardId) {
 	    return boardDao.selectOne(boardId);
 	}
+	
+	//수정
+	@PutMapping("/")
+	public void edit(@RequestBody BoardDto boardDto,
+	        @RequestHeader("Authorization") String bearerToken) {
+	    boardService.edit(boardDto, bearerToken);
+	    System.out.println("게시글 수정 실행");
+	}
+	//삭제
+	@DeleteMapping("/{boardId}")
+	public void delete(@PathVariable int boardId,
+			@RequestHeader("Authorization") String bearerToken) {
+		boardService.delete(boardId, bearerToken);
+		System.out.println("게시글 삭제 실행");
+	}
+	
+	
 }
--- lolinfo/src/main/java/com/lol/lolinfo/service/BoardService.java
+++ lolinfo/src/main/java/com/lol/lolinfo/service/BoardService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 
 import com.lol.lolinfo.dao.BoardDao;
 import com.lol.lolinfo.dto.BoardDto;
+import com.lol.lolinfo.dto.CkDto;
+import com.lol.lolinfo.error.NeedPermissionException;
 import com.lol.lolinfo.error.TargetNotfoundException;
 import com.lol.lolinfo.vo.TokenVO;
 
@@ -26,4 +28,33 @@ public class BoardService {
 		boardDto.setBoardId(boardId);
 		boardDao.insert(boardDto);
 	}
+	
+	//수정
+	public void edit(BoardDto boardDto, String bearerToken) {
+		int boardId = boardDto.getBoardId();
+		requireManagePermission(boardId, bearerToken);
+		boardDao.update(boardDto);
+	}
+	//삭제
+	public void delete(int boardId, String bearerToken) {
+		requireManagePermission(boardId, bearerToken);
+		boardDao.delete(boardId);
+	}	
+	
+	///---------------
+	// 수정·삭제 권한 검사(작성자 or 관리자)
+	private boolean requireManagePermission(int boardId, String bearerToken) {
+		TokenVO tokenVO = tokenService.parse(bearerToken);
+		if(tokenVO == null) throw new TargetNotfoundException();
+	    BoardDto originDto = boardDao.selectOne(boardId);
+	    if (originDto == null) throw new TargetNotfoundException();
+	    // 작성자 또는 관리자 여부 확인
+	    boolean isOwner = tokenVO.getLoginId()
+	            .equals(originDto.getBoardWriter());
+	    boolean isAdmin = "관리자"
+	            .equals(tokenVO.getLoginLevel());
+	    if (!isOwner && !isAdmin) throw new NeedPermissionException();
+	    return true;
+	}
+	
 }
